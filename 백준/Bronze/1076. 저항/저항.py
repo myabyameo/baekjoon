9c25f4613b864106625589bcddeec6ce9bc011e7
myabyameo <148322564+myabyameo@users.noreply.github.com>
2024-09-05 15:05:49 +0900

    [Bronze II] Title: 저항, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,7 @@
+import sys
+input=sys.stdin.readline
+dic={'black':'0', 'brown':'1', 'red':'2','orange':'3','yellow':'4','green':'5','blue':'6','violet':'7','grey':'8','white':'9'}
+a=input().strip()
+b=input().strip()
+c=input().strip()
+print(int(dic[a]+dic[b])*(10**int(dic[c])))
