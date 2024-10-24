fbf3f5ae65627b4b59e21b08be7383d8049432ca
myabyameo <148322564+myabyameo@users.noreply.github.com>
2024-10-24 09:06:47 +0900

    [Bronze IV] Title: 모음의 개수, Time: 92 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.py
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.py
@@ -1,30 +1,10 @@
-k = []
 while True:
-  sum = 0
-  a = input()
-  if a == "#":
-    break
-  else:
-    k = list(a)
-    for i in k:
-      if i == "a":
-        sum += 1
-      elif i == 'e':
-        sum += 1
-      elif i == 'i':
-        sum += 1
-      elif i == 'o':
-        sum += 1
-      elif i == 'u':
-        sum += 1
-      elif i == "A":
-        sum += 1
-      elif i == "E":
-        sum += 1
-      elif i == "I":
-        sum += 1
-      elif i == "O":
-        sum += 1
-      elif i == "U":
-        sum += 1
-  print(sum)
+    q = input()
+    if q == "#":
+        break
+    o = 0
+    for i in q:
+        i=i.lower()
+        if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
+            o += 1
+    print(o)
