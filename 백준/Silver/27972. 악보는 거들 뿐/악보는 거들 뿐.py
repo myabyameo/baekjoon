d724b6a80f333a979d3708eaa618901a05d83842
myabyameo <148322564+myabyameo@users.noreply.github.com>
2024-10-29 12:10:15 +0900

    [Silver I] Title: 악보는 거들 뿐, Time: 236 ms, Memory: 201192 KB -BaekjoonHub

---
+++ 백준/Silver/27972. 악보는 거들 뿐/악보는 거들 뿐.py
@@ -0,0 +1,13 @@
+n=int(input())
+li=list(map(int,input().split()))
+t=[1]*n
+t2=[1]*n
+for i in range(1,n):
+    if li[i]>li[i-1]:
+        t[i]+=t[i-1]
+    if li[i]<li[i-1]:
+        t2[i]+=t2[i-1]
+    if li[i]==li[i-1]:
+        t[i]=t[i-1]
+        t2[i]=t2[i-1]
+print(max(max(t),max(t2)))
