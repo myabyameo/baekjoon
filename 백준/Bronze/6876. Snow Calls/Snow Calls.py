5e398920e44ac35187e47acd0e338bcaac526392
myabyameo <148322564+myabyameo@users.noreply.github.com>
2024-10-29 09:20:42 +0900

    [Bronze II] Title: Snow Calls, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/6876. Snow Calls/Snow Calls.py
@@ -0,0 +1,13 @@
+dic={'A':'2','B':'2','C':'2','D':'3','E':'3','F':'3','G':'4','H':'4','I':'4','J':'5','K':'5','L':'5','M':'6','N':'6','O':'6','P':'7','Q':'7','R':'7','S':'7','T':'8','U':'8','V':'8','W':'9','X':'9','Y':'9','Z':'9'}
+for _ in range(int(input())):
+    s=''.join(input().split('-'))
+    li=[]
+    for i in s:
+        if i.isnumeric():
+            li.append(i)
+        elif i.isalpha():
+            li.append(dic[i])
+
+    li.insert(3,'-')
+    li.insert(7,'-')
+    print(''.join(li)[:12])
